c15df322bea68d0249eed794f6e259ffdcc5b5a8
Parami <paramiashinsana@gmail.com>
2024-08-16 20:00:45 +0530

    update the navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainComponent } from "../main/main.component";
+import { NavigationComponent } from "../app/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
